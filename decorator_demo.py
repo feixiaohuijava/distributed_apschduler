6daeaefb5ac0bcf4d1c313d5cdc8389d250c3862
feixiaohui <feixiaohuijava@163.com>
2018-04-03 10:42:34 +0800

    add demo

--- decorator_demo.py
+++ decorator_demo.py
@@ -1,24 +1,51 @@
-# def deco(func):
-#     def _deco(*args,**kwargs):
-#         print 'before invoked'
-#         result = func(*args,**kwargs)
-#         print 'after invoked'
-#         return result
-#     return _deco
-#
-# @deco
-# def f(a):
-#     print 'f is invoked'
-#     return a+1
-#
-# b = f(1)
-# print b
+""" copy from https://segmentfault.com/a/1190000004238416"""
 
-import datetime
-unknow_time = datetime.timedelta(seconds = 3)
-print unknow_time
-now_time = datetime.datetime.now()
-print now_time
-now =now_time- unknow_time
-print now
+"""
+demo 1
+"""
+def deco(func):
+    def _deco():
+        print 'before invoked'
+        func()
+        print 'after invoked'
+    return _deco
 
+@deco
+def f():
+    print 'f is invoked'
+
+"""
+demo 2
+"""
+def deco(func):
+    def _deco(*args, **kwargs):
+        print 'before invoked'
+        ret = func(*args, **kwargs)
+        print 'after invoded'
+        return ret
+    return _deco
+
+@deco
+def f(a):
+    print 'f is invoked'
+    return a + 1
+
+"""
+demo 3
+"""
+
+def deco(*args):
+    def _deco(func):
+        def __deco(*args, **kwargs):
+            print 'decorator args is', args
+            print 'before invoked'
+            ret = func(*args, **kwargs)
+            print 'after invoded'
+            return ret
+        return __deco
+    return _deco
+
+@deco('test')
+def f(a):
+    print 'f is invoked'
+    return a + 1
